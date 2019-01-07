6132234281e8abae5fedf4aafe6611a5def2f4ab
John Paradise <john.paradise117@gmail.com>
2019-01-07 06:47:46 -0500

    finished part 13

--- Part 13 - Social Company Blog/puppycompanyblog/blog_posts/forms.py
+++ Part 13 - Social Company Blog/puppycompanyblog/blog_posts/forms.py
@@ -8,5 +8,5 @@ from wtforms.validators import DataRequired
 
 class BlogPostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    post = TextAreaField('Text', validators=[DataRequired()])
+    text = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('Submit Post')
