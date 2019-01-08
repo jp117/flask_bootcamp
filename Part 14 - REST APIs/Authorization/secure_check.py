1e56edbd18f84903f3d420e24a085ac292a3827f
John Paradise <john.paradise117@gmail.com>
2019-01-07 19:27:26 -0500

    finished bootcamp

--- Part 14 - REST APIs/Authorization/secure_check.py
+++ Part 14 - REST APIs/Authorization/secure_check.py
@@ -4,8 +4,15 @@ users = [User(1, 'Jose', 'mypassword'),
          User(2, 'Mimi', 'secret')]
 
 username_table = {u.username: u for u in users}
-userid_table = {u.id:u for u in users}
+userid_table = {u.id: u for u in users}
+
 
 def authenticate(username, password):
-    users = username_table.get(username)
+    user = username_table.get(username, None)
+    if user and password == user.password:
+        return user
 
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
--- README.md
+++ README.md
@@ -13,6 +13,6 @@
 ### ~Section 11: Large Flask Applications~
 ### ~Section 12: Flask Migrate for Database Migrations~
 ### ~Section 13: Large Project - Social Company Blog~
-### Section 14: Flask REST APIs
-### Section 15: Deployment
-### Section 16: Payments with Stripe
+### ~Section 14: Flask REST APIs~
+### ~Section 15: Deployment~
+### ~Section 16: Payments with Stripe~
